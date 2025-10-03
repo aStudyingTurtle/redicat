adf29de6b793f20a2770d97c01821a2ce3066b6f
astudyingone <lijiaxuanchn@outlook.com>
2025-10-03 13:15:37 +0800

    fix(bam2mtx): remove trailing space from skip-max-depth default value

--- src/commands/bam2mtx/args.rs
+++ src/commands/bam2mtx/args.rs
@@ -62,7 +62,7 @@ pub struct Bam2MtxArgs {
         long = "skip-max-depth",
         short = "s",
         visible_alias = "sD",
-        default_value = "2147483647 "
+        default_value = "2147483647"
     )]
     pub skip_max_depth: u32,
 
